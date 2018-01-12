7589f66e7b58cd221dba5cd602bd371e25db8802
Pranav Vashi <neobuddy89@gmail.com>
2018-01-12 15:30:07 +0530

    Override DocumentsUI
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- Android.mk
+++ Android.mk
@@ -26,6 +26,7 @@ LOCAL_PACKAGE_NAME := crDroidFileManager
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_OVERRIDES_PACKAGES := DocumentsUI
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
